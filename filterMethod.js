80c026dd360d628afed447ba8bea25038261e85f
Guilherme Guimaraes <Technestowl@gmail.com>
2024-08-28 20:37:00 -0400

    updated star count in galaxy

--- filterMethod.js
+++ filterMethod.js
@@ -9,10 +9,10 @@ document.getElementById("searchBtn").addEventListener("click", toggleView);
 document.getElementById("close-card").addEventListener("click", toggleView);
 
 
+//galaxy background
 const starfield = document.querySelector('.starfield');
 
-//Stars
-for (let i = 0; i < 150; i++) {
+for (let i = 0; i < 500; i++) {
     const star = document.createElement('div');
     star.className = 'star';
     star.style.top = `${Math.random() * 100}vh`;
@@ -185,12 +185,6 @@ const characters = [
     }
 ];
 
-
-
-
-
-
-
 const triggerSearch = () => {   
     let userInput = document.getElementById("charSearchByName").value;//grabing user input ( character name)
 
@@ -235,106 +229,12 @@ charNames.forEach( i => {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//***MAP***
-//1. Get array of all names
-//2. Get array of all species and their homeworld
-//3. Get array of objects with just name and role properties
-//4. Get array of all first names
-
-//1:
-let nameArr = [];
-characters.map((i)=>{
-    nameArr.push(i.name)
-});
-
-//simplier method
-let planets = characters.map(i => i.homeworld);
-
-
-
-
-//2:
-let charType = [];
-characters.map((i)=>{
-    charType.push([i.species,i.homeworld]);
-});
-
-let planetObj = characters.map( (i) => ({
-    species: i.species,
-    homeworld: i.homeworld
-    })
-);
-
-//3:
-let charObj = [];
-characters.map((i)=>{
-    charObj.push({
-        name:i.name,
-        role:i.role,
-    });
-});
-
-
-//4:
-const firstName = characters.map( char => char.name.split(' ')[0]);
-
-
-// console.log(firstNames)
-
-
-//***REDUCE***
-//1. Get total mass of all characters
-//2. Get total height of all characters
-//3. Get total number of characters by eye color
-//4. Get total number of characters in all the character names
-
-//***FILTER***
-//1. Get characters with role Revel Alliance
-//3. Get all droid characters based on their role ( not species )
-
-//1:
 let heros = characters.filter( character => {
 
     if(character.affiliation == "Rebel Alliance"){
         return character; // if condition is true, current character will pass the filter and be added to heros array
     };
 });
-//refactored code above:
 let scum = characters.filter(character => character.affiliation == "Galactic Empire");
 
-//3:
 let droids = characters.filter(char => char.role.includes("Droid"));
-
-
-
-
-//***SORT***
-//1. Sort by mass
-//2. Sort by height
-//3. Sort by name
-//4. Sort by gender
-
-//***EVERY***
-//1. Does every character have blue eyes?
-//2. Does every character have mass more than 40?
-//3. Is every character shorter than 200?
-//4. Is every character male?
-
-//***SOME***
-//1. Is there at least one male character?
-//2. Is there at least one character with blue eyes?
-//3. Is there at least one character taller than 210?
